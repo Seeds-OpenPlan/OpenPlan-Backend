11190b3ea5a76d1139ac5fba1bfe7aaaed236cde
soheeGit <63wlsthgml@gmail.com>
2026-08-22 22:53:36 +0900

    feat: 자동배치 컨피그 추가

--- src/main/java/com/openplan/backend/global/config/RuleEngineConfig.java
+++ src/main/java/com/openplan/backend/global/config/RuleEngineConfig.java
@@ -1,6 +1,8 @@
 package com.openplan.backend.global.config;
 
+import com.openplan.backend.rule.engine.FirstFitPlacementEngine;
 import com.openplan.backend.rule.engine.PlanValidationEngine;
+import com.openplan.backend.rule.port.PlanPlacementPort;
 import com.openplan.backend.rule.port.PlanValidationPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +22,10 @@ public class RuleEngineConfig {
     @Bean
     public PlanValidationPort planValidationPort() {
         return new PlanValidationEngine();
+    }
+
+    @Bean
+    public PlanPlacementPort planPlacementPort() {
+        return new FirstFitPlacementEngine();
     }
 }
